a1344d3f2dbb4e7e2fb16a909b1fcc2b4070e9f7
callmeoli <olikorma@gmail.com>
2022-08-25 16:48:53 +0300

    Hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0 as ad
-a = 1
-b = 2
-sum1 = ad.add(a, b)
-print("{0} + {1} = {2}".format(a, b, sum1))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    sum1 = ad.add(a, b)
+    print("{0} + {1} = {2}".format(a, b, sum1))
