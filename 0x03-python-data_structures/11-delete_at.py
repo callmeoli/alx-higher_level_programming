a81d738b33d00b25205c2e2b2f888ac994c754c5
olana <olikorma@gmaill.com>
2022-08-29 13:32:40 -0700

    11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 and len(my_list) <= idx:
+        return my_list
+    else:
+        my_list.remove(my_list[idx])
+        new_list = my_list
+        return new_list
