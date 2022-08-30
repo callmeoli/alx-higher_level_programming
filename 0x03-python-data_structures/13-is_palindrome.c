cf75dbcb4e6d050a05dad2200bce3d5e1bf7c2c3
olana <olikorma@gmaill.com>
2022-08-30 12:47:37 -0700

    hot fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,6 +10,9 @@ int is_palindrome(listint_t **head)
 listint_t *current;
 listint_t *end;
 int count = 0;
+int k;
+int i = 0;
+int j = 0;
 
 current = *head;
 end = *head;
@@ -19,8 +22,23 @@ while (end->next != NULL)
 	end = end->next;
 	count++;
 }
-if (end->n != current->n)
+end = *head;
+k = count;
+
+while (i != k / 2)
+{
+while (j < count) // we got to the end @count
+{
+	end = end->next;
+	j++;
+}
+count--;
+j = 0;
+if (end->n != current->n) // we compare end with current 
 	return (0);
-else
-	return (1);
+current = current->next; //we got to the next current
+i++;
+end = *head;
+}
+return (1);
 }
--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -17,11 +17,13 @@ int main(void)
     add_nodeint_end(&head, 972);
     add_nodeint_end(&head, 50);
     add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 8);
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 50);
     add_nodeint_end(&head, 972);
     add_nodeint_end(&head, 17);
     add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 3);
     print_listint(head);
 
     if (is_palindrome(&head) == 1)
