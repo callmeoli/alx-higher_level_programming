5894c6d50dcf6b1224d7bc10f634aad55b958060
Callmeoli <olikorma@gmail.com>
2022-09-01 19:50:41 +0300

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def square(x):
-    return x*x
+def square(matrix=[])
+    for i in range(len(x)):
+        for j in range(len(x[i])):
+            return x*x
+
 def square_matrix_simple(matrix=[]):
     new_matrix = map(square, matrix)
     return new_matrix
