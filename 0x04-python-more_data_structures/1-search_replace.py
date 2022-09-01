70f3f5cc4a348016e5fd5ea66c0629bf0243b8bc
olana <olikorma@gmaill.com>
2022-09-01 13:34:01 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is not None:
-        new = []
-        for i in range(0, len(my_list)):
-            if my_list[i] != search:
-                new.append(my_list[i])
-            elif my_list[i] == search:
-                new.append(replace)
-        return new
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
