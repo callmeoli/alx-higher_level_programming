e771ea0e38f58bad8ffeb9011d194983c108ca66
olana <olikorma@gmaill.com>
2022-09-02 11:27:05 -0700

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dic = {"M": 1000, "C": 100, "D": 500, "X": 10, "L": 50, "V": 5, "I": 1}
+    num = 0
+    last = "I"
+    for numeral in roman_string[::-1]:
+        if dic[numeral] < dic[last]:
+            num = num - dic[numeral]
+        else:
+            num = num + dic[numeral]
+    return num
