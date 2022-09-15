0527979e13bf7541b6d4b3591e6a39c967db202d
olana <olikorma@gmaill.com>
2022-09-14 17:54:34 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    This module define square
+"""
+
+
+class Square:
+
+    """ Class Square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Initialize method that stores the size of the square
+
+        Args:
+            param1 (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+
+        """ Return:  The area of square """
+        return (self.__size * self.__size)
