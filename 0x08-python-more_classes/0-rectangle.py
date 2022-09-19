f371445f3432ff41bcf93a0a9cb6a9ed1ebaff52
Callmeoli <olikorma@gmail.com>
2022-09-19 16:34:35 +0300

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Rectangle:
+
+    """ A rectangle with a width and height. """
+
+    def __init__(self):
+        """ (Rectangle, number, number)
+
+        Create a new rectangle of width w and height h.
+
+        >>> r = Rectangle(1, 2)
+        >>> r.width
+        1
+        >>> r.height
+        2
+        """
+
+        # self.width = w
+        # self.height = h
+
+    # def area(self):
+    #     """ (Rectangle) -> number
+
+    #     Return the area of this rectangle.
+        # >>> r = Rectangle(10, 20)
+    #     >>> r.area()
+    #     200
+    #     """
+
+        # return self.width * self.height
