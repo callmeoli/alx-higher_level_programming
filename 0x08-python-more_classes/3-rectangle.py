a4e910e810daf530bbda7b1c560514874f29dc4a
Callmeoli <olikorma@gmail.com>
2022-09-20 16:02:53 +0300

    Hot_fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -7,19 +7,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ (Rectangle, number, number) """
 
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = height
-
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
+        self.__height = height
+        self.__width = width
 
     def __str__(self):
         """ Return the value """
