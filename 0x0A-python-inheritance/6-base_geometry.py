28354573471a55d7ec5ad57f90eef3a4ff1d2c95
Callmeoli <olikorma@gmail.com>
2022-09-26 18:46:16 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module include that print list in sorted order
+"""
+
+
+class BaseGeometry():
+    """
+    This class contain print sorted list
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
