2ab49188b5b19ed5b3b93d91f53d085b6104161b
Callmeoli <olikorma@gmail.com>
2022-09-26 16:51:15 +0300

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" 
+This module looksup at object for the aviable attribute and method 
+"""
+def lookup(obj):
+    """ function return the aviable attribute and method """
+    return dir(obj)
