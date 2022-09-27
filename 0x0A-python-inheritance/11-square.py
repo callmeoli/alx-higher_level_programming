387aa8d8ec56f664f9c139bba1af2b637fa8c37f
Callmeoli <olikorma@gmail.com>
2022-09-27 10:40:33 +0300

    11-main.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module include that print list in sorted order
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class contain print sorted list
+    """
+
+    def __init__(self, size):
+        """ Initializes instance"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """ a method that return the area of the instance """
+        return super().area()
+    def __str__(self):
+        return ("[{}] {:d}/{:d}".format("Square", self.__size , self.__size))
