edff9459793b482ff01585e58b264272f0fc4786
Callmeoli <olikorma@gmail.com>
2022-09-27 11:04:33 +0300

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 class MyInt(int):
     """ Class that inherits from class int"""
+    
+
+    def __init__(self, value):
+    """Initialize value"""
+    self.value = value
 
     def __eq__(self, other):
         """ Method that returns != check """
