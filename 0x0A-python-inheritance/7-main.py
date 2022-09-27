d97b018050a3589f277fc154503d261d6def7abf
Callmeoli <olikorma@gmail.com>
2022-09-27 09:06:03 +0300

    7-base_geometry.txt

--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -6,17 +6,17 @@ bg = BaseGeometry()
 bg.integer_validator("my_int", 12)
 bg.integer_validator("width", 89)
 
-try:
-    bg.integer_validator("name", "John")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+bg.integer_validator("name", "John")
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    bg.integer_validator("age", 0)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+bg.integer_validator("age", 0)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    bg.integer_validator("distance", -4)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+bg.integer_validator("distance", -4)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
