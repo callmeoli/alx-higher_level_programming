054ea2b23025a5f81586108482d4de382397451e
Callmeoli <olikorma@gmail.com>
2022-09-27 09:54:05 +0300

    8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,16 +2,26 @@
 """
 This module include that print list in sorted order
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry():
+class Rectangle(BaseGeometry):
     """
     This class contain print sorted list
     """
+    def __init__(self, width, height):
+        """ Initializes instance"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
     def area(self):
+        """ calculate the area """"
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validate the input """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
