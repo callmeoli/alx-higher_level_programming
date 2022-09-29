efc2099d562d6d7f53ff92e06864fef978de1c71
Callmeoli <olikorma@gmail.com>
2022-09-29 16:28:21 +0300

    python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Handles file input and saves to file"""
+"""Handles file input  and and saves to file"""
 from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
