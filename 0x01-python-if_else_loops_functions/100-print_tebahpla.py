940f2441812c96c52a7f9f05a473e23f6875e97e
Callmeoli <olikorma@gmail.com>
2022-10-23 13:29:32 +0300

    0x01-python

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+a = list(range(65 , 91))
+b = list(range(97, 123))
+i = 0
+print(a)
+while ( i < 26 ):
+
+    # if i % 2 == 0:
+        print(a[i])
+    += 1
+    # else:
+    #     print(b[i])
+    #     i += 1
