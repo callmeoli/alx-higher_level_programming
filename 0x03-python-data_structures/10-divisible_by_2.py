d5b34799261a71b86d10315541777858ca031c97
Callmeoli <olikorma@gmail.com>
2022-10-23 17:04:21 +0300

    python_data_stractures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = []
-    for i in range(0, len(my_list)):
-        if my_list[i] % 2:
-            new_list.append(False)
-        else:
+    for i in my_list:
+        if i % 2 == 0:
             new_list.append(True)
+        else:
+            new_list.append(False)
     return new_list
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j < (len(matrix[i]) - 1):
-                print("{0}".format(" "), end="")
+    for i in matrix:
+        for j in i:
+            if j == i[-1]:
+                print("{:d}".format(j), end="")
             else:
-                print("{0}".format("\n"), end="")
+                print("{:d}".format(j), end=" ")
+        print()
