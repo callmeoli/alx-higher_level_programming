abf1e5f7be5253213bc540739b30b94d4ab4cbdb
Callmeoli <olikorma@gmail.com>
2022-10-23 14:55:52 +0300

    alx

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
+""" this module containg files that describe """
 class Node:
+    """ class node """
     def __init__(self, data, next_node=None):
+        """ init funcion """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """ define data """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """ data setter """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """ next node """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """ next node """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """ singly linked list """
     def __str__(self):
+        """ return string reperesantion """
         rtn = ""
         ptr = self.__head
 
@@ -39,9 +48,11 @@ class SinglyLinkedList:
         return rtn
 
     def __init__(self):
+        """ Initialize init """
         self.__head = None
 
     def sorted_insert(self, value):
+        """ sorted insert """ 
         ptr = self.__head
 
         while ptr is not None:
