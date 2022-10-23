baea34866975cf49704d26a072f166d06a15ea6e
Callmeoli <olikorma@gmail.com>
2022-10-23 18:17:44 +0300

    Hot_fix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,23 @@
-import numpy
+#!/usr/bin/python3.5
+"""
+
+Module composed by a function that multiplies 2 matrices
+
+"""
+import numpy as np
+
 
 def lazy_matrix_mul(m_a, m_b):
-    """Callculate the matrx multiplication of two matrices"""
-    return numpy.matmul(m_a, m_b)
+    """ Function that multiplies 2 matrices
 
+    Args:
+        m_a: matrix a
+        m_b: matrix b
+
+    Returns:
+        result of the multiplication
+
+
+    """
+
+    return (np.matmul(m_a, m_b))
