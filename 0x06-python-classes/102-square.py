6972b4659179f35b47000fe63f6de2ec380037c2
Callmeoli <olikorma@gmail.com>
2022-10-23 17:46:00 +0300

    hot_fix

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,53 +1,87 @@
 #!/usr/bin/python3
+"""a square class"""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
-    def __eq__(self, other):
-        return self.__size == other.__size
-
-    def __lt__(self, other):
-        return self.__size < other.__size
-
-    def __le__(self, other):
-        return self.__size <= other.__size
-
-    def __ne__(self, other):
-        return self.__size != other.__size
-
-    def __gt__(self, other):
-        return self.__size > other.__size
-
-    def __ge__(self, other):
-        return self.__size >= other.__size
-
+    """Derives a square """
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """Initializes the data
+        Args:
+            size (int): size of the square
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+        Raises:
+            TypeError: when `size` isn't an integer
+            ValueError: `size` is less than 0
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if (size < 0):
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
+
+    def __lt__(self, other):
+        """Compare less than of current instance and other instance"""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """
+        Compare less than or equal to of current instance
+        and other instance
+        """
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        """Compare equality of current instance and other instance"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """
+        Compare not equal of current instance
+        and other instance
+        """
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        """
+        Compare greater than of current instance
+        and other instance
+        """
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """
+        Compare greater than or equal to of current instance
+        and other instance
+        """
+        return self.area() >= other.area()
 
     def area(self):
-        """ Method that returns the square are of the object
+        """Calculates the area of a square
+        Returns: the area of the square
         """
+
         return (self.__size ** 2)
 
     @property
     def size(self):
-        """ Method to returns the size value
-        """
+        """Retrieves the value of `size`"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
+        """Sets the value of `value`
+        Args:
+            value (int): value to be set to `value`
+        Raise:
+            TypeError: when `value` isn't an integer
+            ValueError: `value` is less than 0
         """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if (value < 0):
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+
+        self.__size = value
