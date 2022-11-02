89c21b08f6f5d6f38257661c8a6d5369f921a047
root <root@DESKTOP-RSATRMN.localdomain>
2022-11-02 17:00:12 +0300

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- create database and insert table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states( id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL );
