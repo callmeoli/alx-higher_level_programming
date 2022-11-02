0a3f31b3203265ae8b2bca8d0b3bef05a823b85b
root <root@DESKTOP-RSATRMN.localdomain>
2022-11-02 16:14:39 +0300

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,5 @@
 -- this script creat user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT PRIVILEGES ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
