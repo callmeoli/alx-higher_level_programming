f4ffbcfe51ffd0857d86c6da9c485b2100e9c9e7
callmeoli <olikorma@gmail.com>
2022-11-29 16:19:43 +0300

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let j = 0;
+    let S = '';
+    while (i < this.height) {
+      while (j < this.width) {
+        S = S + 'X';
+        j++;
+      }
+      console.log(S);
+      S = '';
+      j = 0;
+      i++;
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
