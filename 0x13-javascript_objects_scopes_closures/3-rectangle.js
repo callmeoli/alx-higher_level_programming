b1310df331db4584f2e1656eca71a5af47b57033
callmeoli <olikorma@gmail.com>
2022-11-29 16:13:13 +0300

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    let j = 0;
+    let S = '';
+    while (i < this.height) {
+      while (j < this.width) {
+        S = S + 'X';
+        j++;
+      }
+      console.log(S);
+      S = '';
+      j = 0;
+      i++;
+    }
+  }
+}
+module.exports = Rectangle;
