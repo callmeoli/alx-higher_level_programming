9a7dd79e24e8f5d4ca901e57997f6dc281907e34
callmeoli <olikorma@gmail.com>
2022-11-29 17:05:37 +0300

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const Square1 = require('./5-Square');
+class Square extends Square1 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let i = 0;
+    let j = 0;
+    let S = '';
+    let symbol = c;
+    if (c === undefined) {
+      symbol = 'X';
+    }
+    while (i < this.size) {
+      while (j < this.size) {
+        S = S + symbol;
+        j++;
+      }
+      console.log(S);
+      S = '';
+      j = 0;
+      i++;
+    }
+  }
+}
+module.exports = Square;
