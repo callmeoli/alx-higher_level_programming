e7d4945932df7f08dfcc5f23741a48d087cad7c0
callmeoli <olikorma@gmail.com>
2022-11-30 16:28:54 +0300

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,19 @@
 #!/usr/bin/node
 const x = process.argv;
-console.log(x);
+const b = x.length;
+let max = parseInt(process.argv[2], 10);
+let max2 = parseInt(process.argv[2], 10);
+
+if (b <= 3) {
+  console.log(0);
+} else {
+  for (let i = 3; i <= b; i++) {
+    if (process.argv[i] > max) {
+      max2 = max;
+      max = process.argv[i];
+    } else if (process.argv[i] > max2) {
+      max2 = process.argv[i];
+    }
+  }
+  console.log(max2);
+}
