273cb8aa53dece433143b56717c596ecdb31eeb4
olana <olikorma@gmaill.com>
2022-12-03 09:04:00 -0800

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
+"""
+This script will connect to database by
+passed user passwd and database
+and print elementes orderd by id
+"""
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(db=sys.argv[3], host="localhost",
-                     user=sys.argv[1], passwd=sys.argv[2], port=3306)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id")
+if __name__ == '__main__':
+    """
+    Access the database and
+    print elments
+    """
+    db = MySQLdb.connect(db=sys.argv[3], host="localhost",
+                         user=sys.argv[1], passwd=sys.argv[2], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
 
-result = cursor.fetchall()
-for row in result:
-    print(row)
-db.close()
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    db.close()
