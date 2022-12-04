f88fb47f1d024bfce93cd9b20ed82b6a9ecbcae7
olana <olikorma@gmaill.com>
2022-12-04 11:46:22 -0800

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,7 +9,7 @@ from model_city import City
 
 if __name__ == "__main__":
     """ This is the code implem"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
