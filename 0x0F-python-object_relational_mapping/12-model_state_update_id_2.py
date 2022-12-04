bdee31482fca41de5fed44e4276f23aa0d07b4a9
olana <olikorma@gmaill.com>
2022-12-04 10:08:51 -0800

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,10 +10,10 @@ from sqlalchemy import update
 if __name__ == "__main__":
     """ prints a conatining seletions"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]))
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     upd = update(State)
-    val = upd.values({"name": "New Mexico"})
+    val = upd.values({State.name: "New Mexico"})
     cond = val.where(State.c.id == 2)
