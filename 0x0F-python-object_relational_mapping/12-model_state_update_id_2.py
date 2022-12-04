2f2f1e1cc0da32855e2ed26cfe2e7f35e392ed14
olana <olikorma@gmaill.com>
2022-12-04 10:19:58 -0800

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,8 +13,7 @@ if __name__ == "__main__":
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
+    stateUpdated = session.query(State).filter(State.id == 2).first()
     if stateUpdated:
-        stateUpdated = session.query(State).filter(State.id == 2).first()
-        upd = update(State)
-        val = upd.values({State.name: "New Mexico"})
-        cond = val.where(State.c.id == 2)
+        stateUpdated.name = "New Mexico"
+        session.commit()
