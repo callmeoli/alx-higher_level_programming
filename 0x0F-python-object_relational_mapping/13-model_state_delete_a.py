d8f4c7fc1ff22a766ed1e9f320003fbcad1d958d
olana <olikorma@gmaill.com>
2022-12-04 10:38:49 -0800

    13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy import update
+
+if __name__ == "__main__":
+    """ prints a conatining seletions"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    stateUpdated = session.query(State).\
+        filter(State.name.contains('a')).delete()
+    session.commit()
