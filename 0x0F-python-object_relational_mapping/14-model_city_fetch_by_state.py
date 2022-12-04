93c3067ae81ac318057ca3872d05391c19d5b827
olana <olikorma@gmaill.com>
2022-12-04 11:44:20 -0800

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,6 +8,7 @@ from sqlalchemy import (create_engine)
 from model_city import City
 
 if __name__ == "__main__":
+    """ This is the code implem"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
