8ee500b87e274b9c7ffe3198bf4032d500a92ef4
olana <olikorma@gmaill.com>
2022-12-04 11:52:54 -0800

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ This module create a table States """
-from sqlalchemy import Integer, Column, String
+from sqlalchemy import Integer, Column, String, Foreignkey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
