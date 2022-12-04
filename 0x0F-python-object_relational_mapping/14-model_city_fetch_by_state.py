1d541c9d7e88f6719f337878ba28a98aba9a6300
olana <olikorma@gmaill.com>
2022-12-04 11:49:13 -0800

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     """ This is the code implem"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states_inst = session.query(City, State)
+    states_inst = session.query(State, City)
     .filter(City.state_id == State.id).all()
     for state, city in states_inst:
         print('{0}: ({1}) {2}'.format(state.name, city.id, city.name))
