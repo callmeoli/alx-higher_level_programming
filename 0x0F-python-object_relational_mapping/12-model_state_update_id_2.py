e2fa3f12ff041792a690b274284f09cb3369ca85
olana <olikorma@gmaill.com>
2022-12-04 09:54:35 -0800

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy import update
+
+if __name__ == "__main__":
+    """ prints a conatining seletions"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    upd = update(State)
+    val = upd.values({"name": "New Mexico"})
+    cond = val.where(State.c.id == 2)
+    states_inst = session.query(State).order_by(State.id)
+    for instance in states_inst:
+        print('{0}: {1}'.format(instance.id, instance.name))
