59e320f14edbb4634f1956bd6c89181a6b8fe878
callmeoli <olikorma@gmail.com>
2022-12-15 17:50:05 +0300

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Bash script that send request to url desplaysize of response
 curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that send request to url desplaysize of response
-curl -sL -X GET "$1"
+curl -sL -X DELETE "$1"
