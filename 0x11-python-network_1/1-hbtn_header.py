252e8199d634f5e406870c8f24f1152269632d91
callmeoli <olikorma@gmail.com>
2022-12-17 15:40:12 +0300

    hot_fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     url = argv[1]
     with request.urlopen(url) \
             as response:
-        Id = response.gerheader('X-Request-Id')
+        Id = response.getheader('X-Request-Id')
     print(Id)
