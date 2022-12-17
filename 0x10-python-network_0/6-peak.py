b3131098bb435c28811b3a39bd72d7e21e872773
callmeoli <olikorma@gmail.com>
2022-12-17 10:54:15 +0300

    python_networl

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,12 +5,11 @@
 def find_peak(list_of_integers):
     """ The function that returns peak """
 
-    if str(list_of_integers) == "[]":
+    if type(list_of_integers) != list:
+        return
+
+    if len(list_of_integers) == 0:
         return None
     else:
-        i = 0
-        max_inx = i
-        for i in range(len(list_of_integers)):
-            if list_of_integers[i] > list_of_integers[max_inx]:
-                max_inx = i
-        return list_of_integers[max_inx]
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
