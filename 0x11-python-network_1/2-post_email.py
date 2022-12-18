3c82bd3e81f5945fb872d03f3b2c3d207312f4bf
callmeoli <olikorma@gmail.com>
2022-12-18 10:06:32 +0300

    hot_fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ take url and display  of X-Reques-Id """
 import sys
-import urllib
+from urllib import request, parse
 
 url = sys.argv[1]
 values = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(values)
+data = parse.urlencode(values)
 data = data.encode('ascii')
-req = urllib.request.Request(url, data)
+req = request.Request(url, data)
 if __name__ == "__main__":
     """ take url and display X-Reques-Id """
-    with urllib.request.urlopen(req) \
+    with request.urlopen(req) \
             as response:
         page = response.read()
     print(page)
