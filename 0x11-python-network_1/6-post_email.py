cfff712b5cf16f230aa282fbf26f322b4520d9cb
callmeoli <olikorma@gmail.com>
2022-12-18 12:05:55 +0300

    hot_fix

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""post email to url provided"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    """ fuction for module """
+    r = requests.post(argv[1], data={'email':argv[2]})
+    print(r.text)
