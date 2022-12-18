bcf7338e1b9acc6d427cd04abbd34b14f81bb02e
callmeoli <olikorma@gmail.com>
2022-12-18 14:16:55 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,18 +7,15 @@ import requests
 
 if __name__ == "__main__":
     """ the function here bellow"""
-    if len(argv) == 1:
-        req = requests.post('http://0.0.0.0:5000/search_user', data={q=""})
-        if (type(req) == 'json' and req is not None):
-            for k, v in req:
-                print('[{}] {}'.format(k, v))
+    q_value = "" if len(argv) == 1 else argv[1]
+    values = {'q'=q_value }
+    url = http://0.0.0.0:5000/search_user
+    response = requests.post(url, values)
+    try:
+        json = resp.json()
+        if json:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
         else:
             print("No result")
-    else:
-        req = requests.post('http://0.0.0.0:5000/search_user',
-                            data={q=argv[1]})
-        if (type(req) == 'json' and req is not None):
-            for k, v in req:
-                print('[{}] {}'.format(k, v))
-        else:
-            print("No result")
+    except Exception:
+        print("Not a valid JSON")
