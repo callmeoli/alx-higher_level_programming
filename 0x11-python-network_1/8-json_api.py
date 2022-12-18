ac1638aef2cb4ce99c6693fd82233ef0071cba01
callmeoli <olikorma@gmail.com>
2022-12-18 13:35:19 +0300

    hot_fix

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Py script that take letter as param and sent it to web
+    http://0.0.0.0:5000/search_user
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    """ the function here bellow"""
+    if len(argv) == 1:
+        req = requests.post('http://0.0.0.0:5000/search_user', data={'q'=""})
+        if (type(req) == 'json' and req is not None):
+            for k, v in req:
+                print('[{}] {}'.format(k, v))
+        else:
+            print("No result")
+    else:
+        req = requests.post('http://0.0.0.0:5000/search_user',
+                            data={'q'=argv[1]})
+        if (type(req) == 'json' and req is not None):
+            for k, v in req:
+                print('[{}] {}'.format(k, v))
+        else:
+            print("No result")
