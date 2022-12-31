5f754413e19d58d02a881ef6186053e3d43825a6
allmeoli <olikorma@gmail.com>
2022-12-31 15:35:03 +0300

    model_

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ This module create a table States """
+
+
 from sqlalchemy import Integer, Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
