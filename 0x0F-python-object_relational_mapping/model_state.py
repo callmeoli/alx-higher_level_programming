c74a6d089a682656feabb59cf78975ae13122a74
allmeoli <olikorma@gmail.com>
2022-12-31 09:29:58 +0300

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-""" This module create a table States """
-from sqlalchemy import Integer, Column, String
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ define table states """
+    """ definition of class state """
+
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
