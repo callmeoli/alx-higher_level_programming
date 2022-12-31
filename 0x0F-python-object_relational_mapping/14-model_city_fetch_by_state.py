bd935521355f4e5177feb8b9887716a2ade35df9
allmeoli <olikorma@gmail.com>
2022-12-31 15:28:22 +0300

    model_city_fetch_by_state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""Start link class to table in database
 """
-from sys import argv
+Prints all City objects from the database hbtn_0e_14_usa
+"""
+import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    """ This is the code implem"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]))
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states_inst = session.query(State, City)\
-    .filter(City.state_id == State.id).all()
-    for state, city in states_inst:
-        print("{0}: ({1}) {2}".format(state.name, city.id, city.name))
+
+    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+
+    for state, city in st_cty:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
