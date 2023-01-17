63ac731721ba24ab7c0952c7abd5417768e2f25e
root <olikorma@gmail.com>
2023-01-17 23:20:07 +0000

    satau_code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* a script that reads and prints the content of a file */
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+  } else {
+    console.log('Status code: ', response.statusCode);
+  }
+});
